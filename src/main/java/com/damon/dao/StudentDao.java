1ce23942bd46aa355b928d9563588ffe0d72205f
Damon <848680491@qq.com>
2021-12-15 10:29:25 +0800

    SSMIntegrationDemo:复习SSM时做的一个小demo，用来理解SSM的用法

---
+++ src/main/java/com/damon/dao/StudentDao.java
@@ -0,0 +1,45 @@
+package com.damon.dao;
+
+import com.damon.bean.Student;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface StudentDao {
+        /**
+         * 获取学生总数
+         * @return
+         */
+        int getTotal();
+
+        /**
+         * 添加一个学生
+         * @param student
+         */
+        void addStudent(Student student);
+
+        /**
+         * 根据 id 删除一个人学生
+         * @param id
+         */
+        void deleteStudent(int id);
+
+        /**
+         * 修改一个学生信息
+         * @param student
+         */
+        void updateStudent(Student student);
+
+        /**
+         * 根据 id 获取一个学生信息
+         * @param id
+         * @return
+         */
+        Student getStudent(int id);
+
+        /**
+         * 查询从start位置开始的count条数据
+         */
+        List<Student> list(int start, int count);
+}
